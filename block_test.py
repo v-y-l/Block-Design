2aec6a9622355c07936ec7547929bacc6d718583
Victor Lin <vlinvlin6@gmail.com>
2020-10-14 23:45:27 -0400

    Amend tests

--- block_test.py
+++ block_test.py
@@ -1,19 +1,36 @@
 import unittest
 
-from block import Block
+from block import Block, BlockOrientation
 
 class TestBlockMethods(unittest.TestCase):
 
     def test_flipright(self):
         block = Block()
+        self.assertEqual(block.face[1], BlockOrientation.Up)
         self.assertEqual(block.face[0], 1)
         block.flipRight()
+        self.assertEqual(block.face[1], BlockOrientation.Left)
         self.assertEqual(block.face[0], 2)
         block.flipRight()
+        self.assertEqual(block.face[1], BlockOrientation.Down)
         self.assertEqual(block.face[0], 6)
         block.flipRight()
+        self.assertEqual(block.face[1], BlockOrientation.Right)
         self.assertEqual(block.face[0], 4)
 
+    def test_flipsequenceone(self):
+        block = Block()
+        self.assertEqual(block.face[1], BlockOrientation.Up)
+        self.assertEqual(block.face[0], 1)
+        block.flipLeft()
+        self.assertEqual(block.face[1], BlockOrientation.Right)
+        self.assertEqual(block.face[0], 4)
+        block.flipDown()
+        self.assertEqual(block.face[1], BlockOrientation.Right)
+        self.assertEqual(block.face[0], 5)
+        block.flipRight()
+        self.assertEqual(block.face[1], BlockOrientation.Left)
+        self.assertEqual(block.face[0], 1)
 
 if __name__ == '__main__':
     unittest.main()
