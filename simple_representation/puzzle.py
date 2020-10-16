afdd38004bc1bcd8b2bfe9a0276127152e1e6b56
Victor Lin <vlinvlin6@gmail.com>
2020-10-16 11:55:04 -0400

    Add one strategy

--- simple_representation/puzzle.py
+++ simple_representation/puzzle.py
@@ -1,9 +1,28 @@
 from block import Pattern, Block
+from random import sample
 
 class Puzzle:
 
-    def __init__(self, dest):
+    def __init__(self, dest, searcher):
+        self.searcher = searcher
         self.dest = dest
         self.blocks = len(dest)*[0]
         for i, _ in enumerate(dest):
             dest[i] = Block()
+
+    def solve(self):
+        moves = []
+        for i in range(len(dest)):
+            moves.extend(
+                self.searcher(self.blocks[i], self.dest[i]))
+        return moves
+
+def random_search(block, dest_pattern):
+    moves = []
+    if block.getPattern() == dest_pattern:
+        return moves
+
+    next_face = sample(block.getNeighbors(), 1)[0]
+    moves.append(next_face)
+    block.goToFace(next_face)
+    return random_search(block, dest_pattern)
--- simple_representation/puzzle_test.py
+++ simple_representation/puzzle_test.py
@@ -1,14 +1,25 @@
 import unittest
 
-from block import Pattern
-from puzzle import Puzzle
+from block import Pattern, Block
+from puzzle import Puzzle, random_search
 
 class TestBlockMethods(unittest.TestCase):
 
     def test_init(self):
         puzzle = Puzzle(
-            [Pattern.Triangle, Pattern.Triangle, Pattern.Triangle, Pattern.Triangle])
+            [Pattern.Triangle, Pattern.Triangle, Pattern.Triangle, Pattern.Triangle],
+            random_search)
         self.assertEqual(len(puzzle.blocks), 4)
+
+    def test_random_search(self):
+        block = Block()
+        self.assertEqual(block.getFace(), 1)
+        self.assertEqual(block.getPattern(), Pattern.Triangle)
+        moves = random_search(block, Pattern.White)
+        for m in moves:
+            block.goToFace(m)
+        self.assertEqual(block.getPattern(), Pattern.White)
+
         
 if __name__ == '__main__':
     unittest.main()
