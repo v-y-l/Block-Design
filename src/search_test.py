cb7a0af34a3294c8b83053e01f0b63121f6eb4f0
Victor Lin <vlinvlin6@gmail.com>
2020-11-14 23:56:28 -0500

    Add beeline search test

--- src/search_test.py
+++ src/search_test.py
@@ -13,7 +13,7 @@ class TestSearchMethods(unittest.TestCase):
             block.executeAction(action)
         self.assertEqual(block.getPattern(), BlockPattern.BlackTopLeftCornerSquare)
 
-    def test_beeline_search(self):
+    def test_beeline_search_rotations(self):
         print('\Beeline search - rotations')
         actions = beeline_search(Block(), BlockPattern.BlackTopLeftCornerSquare, [])
         block = Block()
@@ -25,11 +25,21 @@ class TestSearchMethods(unittest.TestCase):
         actions = beeline_search(Block(), BlockPattern.BlackBottomLeftCornerSquare, [])
         block = Block()
         self.assertEqual(block.getPattern(), BlockPattern.BlackTopRightCornerSquare)
-        # Since beeline search is somewhat stochastic, these test are heuristics
+        # Since beeline search is somewhat stochastic, these test rely on heuristics
         self.assertEqual(len(actions), 2)
         for action in actions:
             block.executeAction(action)
         self.assertEqual(block.getPattern(), BlockPattern.BlackBottomLeftCornerSquare)
+
+    def test_beeline_search_complex(self):
+        print('\Beeline search - complex')
+        actions = beeline_search(Block(3), BlockPattern.BlackTopLeftCornerSquare, [])
+        block = Block(3)
+        self.assertEqual(block.getPattern(), BlockPattern.WhiteSquare)
+        self.assertTrue(len(actions) == 2)
+        for action in actions:
+             block.executeAction(action)
+        self.assertEqual(block.getPattern(), BlockPattern.BlackTopLeftCornerSquare)
 
 if __name__ == '__main__':
     unittest.main()
