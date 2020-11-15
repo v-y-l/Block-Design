bc59362fcaa72d26a464f6e04b2d75ccc0f7fead
Victor Lin <vlinvlin6@gmail.com>
2020-11-14 23:47:00 -0500

    Add beeline search tests

--- src/search_test.py
+++ src/search_test.py
@@ -1,7 +1,7 @@
 import unittest
 
 from block import BlockPattern, Block
-from search import random_search
+from search import random_search, beeline_search
 
 class TestSearchMethods(unittest.TestCase):
 
@@ -12,6 +12,24 @@ class TestSearchMethods(unittest.TestCase):
         for action in actions:
             block.executeAction(action)
         self.assertEqual(block.getPattern(), BlockPattern.BlackTopLeftCornerSquare)
+
+    def test_beeline_search(self):
+        print('\Beeline search - rotations')
+        actions = beeline_search(Block(), BlockPattern.BlackTopLeftCornerSquare, [])
+        block = Block()
+        self.assertEqual(block.getPattern(), BlockPattern.BlackTopRightCornerSquare)
+        self.assertEqual(len(actions), 1)
+        block.executeAction(actions[0])
+        self.assertEqual(block.getPattern(), BlockPattern.BlackTopLeftCornerSquare)
+
+        actions = beeline_search(Block(), BlockPattern.BlackBottomLeftCornerSquare, [])
+        block = Block()
+        self.assertEqual(block.getPattern(), BlockPattern.BlackTopRightCornerSquare)
+        # Since beeline search is somewhat stochastic, these test are heuristics
+        self.assertEqual(len(actions), 2)
+        for action in actions:
+            block.executeAction(action)
+        self.assertEqual(block.getPattern(), BlockPattern.BlackBottomLeftCornerSquare)
 
 if __name__ == '__main__':
     unittest.main()
