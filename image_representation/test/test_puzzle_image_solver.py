8b42371c1e126b1e60d64a6f809ed94cbb63d6cc
Victor Lin <vlinvlin6@gmail.com>
2020-11-28 18:55:18 -0500

    Complete tests

--- image_representation/test/test_puzzle_image_solver.py
+++ image_representation/test/test_puzzle_image_solver.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 from utils.enums import BlockPattern
+from block_image import BlockImage
 from puzzle_image_solver import PuzzleImageSolver, SearchType, random_search, sequential_search
 
 class TestPuzzleImageSolver(unittest.TestCase):
@@ -105,37 +106,38 @@ class TestPuzzleImageSolver(unittest.TestCase):
         puzzle_solver = PuzzleImageSolver('./puzzle_images/puzzle_c.png')
         self.assertEqual(len(puzzle_solver.blockBank), 9)
 
-    # def test_random_search(self):
-    #     print('\nApply random search for a single block')
-    #     block = Block(1, 1)
-    #     self.assertEqual(block.getFace(), 1)
-    #     self.assertEqual(block.getPattern(),
-    #                      BlockPattern.BlackTopRightCornerSquare)
-    #     random_search(block, BlockPattern.WhiteSquare, [])
-    #     self.assertEqual(block.getPattern(), BlockPattern.WhiteSquare)
+    def test_random_search(self):
+        print('\nApply random search for a single block')
+        block = BlockImage(1, 1)
+        self.assertEqual(block.getFace(), 1)
+        self.assertEqual(block.getPattern(),
+                         BlockPattern.BlackTopRightCornerSquare)
+        random_search(block, BlockPattern.WhiteSquare, [])
+        self.assertEqual(block.getPattern(), BlockPattern.WhiteSquare)
 
-    # def test_random_search_puzzle(self):
-    #     print('\nApply random search for the puzzle')
-    #     expected_patterns = [
-    #         BlockPattern.BlackSquare,
-    #         BlockPattern.BlackBottomLeftCornerSquare,
-    #         BlockPattern.BlackTopRightCornerSquare,
-    #         BlockPattern.BlackSquare
-    #     ]
-    #     puzzle_solver = PuzzleSolver([
-    #         BlockPattern.BlackSquare,
-    #         BlockPattern.BlackBottomLeftCornerSquare,
-    #         BlockPattern.BlackTopRightCornerSquare,
-    #         BlockPattern.BlackSquare
-    #     ], {
-    #         SearchType.Face: random_search,
-    #         SearchType.PuzzlePiece: sequential_search
-    #     })
-    #     puzzle_solver.solve()
-    #     actual_patterns = []
-    #     for block in puzzle_solver.blockBank:
-    #         actual_patterns.append(block.getPattern())
-    #     self.assertEqual(actual_patterns, expected_patterns)            
+    def test_random_search_puzzle(self):
+        print('\nApply random search for the puzzle')
+        expected_patterns = [
+            BlockPattern.WhiteSquare,
+            BlockPattern.BlackSquare,
+            BlockPattern.BlackTopRightCornerSquare,
+            BlockPattern.BlackSquare,
+            BlockPattern.WhiteSquare,
+            BlockPattern.WhiteSquare,
+            BlockPattern.WhiteSquare,
+            BlockPattern.BlackSquare,
+            BlockPattern.BlackBottomRightCornerSquare,
+        ]
+        puzzle_solver = PuzzleImageSolver(
+            './puzzle_images/puzzle_c.png', {
+            SearchType.Face: random_search,
+            SearchType.PuzzlePiece: sequential_search
+        })
+        puzzle_solver.solve()
+        actual_patterns = []
+        for block in puzzle_solver.blockBank:
+            actual_patterns.append(block.getPattern())
+        self.assertEqual(actual_patterns, expected_patterns)            
 
 if __name__ == '__main__':
     unittest.main()
