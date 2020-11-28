d6bcb0c4695168e68d1bbd593848d2340f8a3582
Victor Lin <vlinvlin6@gmail.com>
2020-11-28 15:46:55 -0500

    Fix some test issues

--- image_representation/utils/block_actions.py
+++ image_representation/utils/block_actions.py
@@ -13,14 +13,14 @@ def goToFace(block, next_face):
 
 ''' Change orientation, but stay on the same face. '''
 def rotateRight(block):
-    block.block_image = rot90(block.image, 3)
+    block.image = rot90(block.image, 3)
     block.orientation = block.blockOrientations[block.orientation].prev.val
     block.r, block.c = block.face_to_coordinate[block.orientation][block.face]
     printAction('[Right rotate block] ', block)
         
 ''' Change orientation, but stay on the same face. '''
 def rotateLeft(block):
-    block.block_image = rot90(block.image)
+    block.image = rot90(block.image)
     block.orientation = block.blockOrientations[block.orientation].next.val
     block.r, block.c = block.face_to_coordinate[block.orientation][block.face]
     printAction('[Left rotate block] ', block)
