70882d14133ff92648db1090c6eac1b1fa19cf10
Victor Lin <vlinvlin6@gmail.com>
2020-11-28 13:09:52 -0500

    Add face to coordinate mapping

---
+++ image_representation/utils/mappings.py
@@ -0,0 +1,48 @@
+from enum import Enum
+
+'''
+Represents the possible orientations of the block.
+'''
+class BlockOrientation(Enum):
+    Up = 1
+    Right = 2
+    Down = 3
+    Left = 4
+
+'''
+Coordinates are represented row by column.
+'''
+face_to_coordinate = {
+    BlockOrientation.Up: {
+        1: (0, 170),
+        2: (170, 0),
+        3: (170, 170),
+        4: (170, 340),
+        5: (170, 510),
+        6: (340, 170)
+    },
+    BlockOrientation.Right: {
+        1: (170, 340),
+        2: (0, 170),
+        3: (170, 170),
+        4: (340, 170),
+        5: (510, 170),
+        6: (170, 0),
+    },
+    BlockOrientation.Down: {
+        1: (340, 340),
+        2: (170, 510),
+        3: (170, 340),
+        4: (170, 170),
+        5: (170, 0),
+        6: (0, 340),
+    },
+    BlockOrientation.Left: {
+        1: (340, 0),
+        2: (510, 170),
+        3: (340, 170),
+        4: (170, 170),
+        5: (0, 170),
+        6: (340, 340),
+    },
+}
