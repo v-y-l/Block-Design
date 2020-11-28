87f1557073e90faf7435befecfd0cbd6a6460c37
Victor Lin <vlinvlin6@gmail.com>
2020-11-28 18:41:53 -0500

    Move working image rep tests to test folder

--- image_representation/test/test_search.py
+++ image_representation/test/test_search.py
@@ -1,29 +1,29 @@
 import unittest
 
-from block import BlockPattern, Block
+from block_image import BlockPattern, BlockImage
 from search import random_search, beeline_search
 
 class TestSearchMethods(unittest.TestCase):
 
     def test_random_search(self):
         print('\nRandom search')
-        actions = random_search(Block(), BlockPattern.BlackTopLeftCornerSquare, [])
-        block = Block()
+        actions = random_search(BlockImage(), BlockPattern.BlackTopLeftCornerSquare, [])
+        block = BlockImage()
         for action in actions:
             block.executeAction(action)
         self.assertEqual(block.getPattern(), BlockPattern.BlackTopLeftCornerSquare)
 
     def test_beeline_search_rotations(self):
         print('\Beeline search - rotations')
-        actions = beeline_search(Block(), BlockPattern.BlackTopLeftCornerSquare, [])
-        block = Block()
+        actions = beeline_search(BlockImage(), BlockPattern.BlackTopLeftCornerSquare, [])
+        block = BlockImage()
         self.assertEqual(block.getPattern(), BlockPattern.BlackTopRightCornerSquare)
         self.assertEqual(len(actions), 1)
         block.executeAction(actions[0])
         self.assertEqual(block.getPattern(), BlockPattern.BlackTopLeftCornerSquare)
 
-        actions = beeline_search(Block(), BlockPattern.BlackBottomLeftCornerSquare, [])
-        block = Block()
+        actions = beeline_search(BlockImage(), BlockPattern.BlackBottomLeftCornerSquare, [])
+        block = BlockImage()
         self.assertEqual(block.getPattern(), BlockPattern.BlackTopRightCornerSquare)
         # Since beeline search is somewhat stochastic, these test rely on heuristics
         self.assertEqual(len(actions), 2)
@@ -33,8 +33,8 @@ class TestSearchMethods(unittest.TestCase):
 
     def test_beeline_search_complex(self):
         print('\Beeline search - complex')
-        actions = beeline_search(Block(3), BlockPattern.BlackTopLeftCornerSquare, [])
-        block = Block(3)
+        actions = beeline_search(BlockImage(3), BlockPattern.BlackTopLeftCornerSquare, [])
+        block = BlockImage(3)
         self.assertEqual(block.getPattern(), BlockPattern.WhiteSquare)
         self.assertTrue(len(actions) == 2)
         for action in actions:
