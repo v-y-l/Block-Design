bb6b47b1fe40ba0d94bafb16074d030616639e81
Victor Lin <vlinvlin6@gmail.com>
2020-12-27 17:17:53 -0500

    Update tests

--- src/test/test_results_analyzer.py
+++ src/test/test_results_analyzer.py
@@ -13,9 +13,13 @@ class TestResultsAnalyzerMethods(unittest.TestCase):
         analyzer.analyze()
         self.assertEqual(analyzer.metadata["num_pieces"], 9)
         self.assertEqual(analyzer.stats["num_runs"], 3)
+        self.assertEqual(analyzer.stats["min_actions"], 19)
         self.assertEqual(analyzer.stats["mean_actions"], 19)
+        self.assertEqual(analyzer.stats["max_actions"], 19)
         self.assertEqual(analyzer.stats["mean_actions_per_block"], 1.89)
+        self.assertEqual(analyzer.stats["min_look_at_puzzle_actions"], 2)
         self.assertEqual(analyzer.stats["mean_look_at_puzzle_actions"], 2)
+        self.assertEqual(analyzer.stats["max_look_at_puzzle_actions"], 2)
         self.assertEqual(analyzer.stats["std_dev_actions"], 0)
         self.assertEqual(analyzer.stats["std_dev_look_at_puzzle_actions"],
                          0)
